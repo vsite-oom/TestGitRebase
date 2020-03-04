79f817ff6ddf4b17e390f20462dcce40f6bedf3d
Senad Mandžić <semandzi@vsite.hr>
2020-03-04 20:03:17 +0100

    Add method with my name

--- TestGitRebase/Program.cs
+++ TestGitRebase/Program.cs
@@ -10,6 +10,21 @@ namespace TestGitRebase
     {
         static void Main(string[] args)
         {
+            JulijanSribar();
+
+            Console.ReadKey(false);
+        }
+
+        static void JulijanSribar()
+        {
+            Console.WriteLine("Course title: OOM");
+            Console.WriteLine("My Name is Julijan Šribar.");
+        }
+
+        static void SenadMandzic()
+        {
+            Console.WriteLine("Course title: OOM");
+            Console.WriteLine("My Name is Senad Mandzic.");
         }
     }
 }

53752118b19230866e7fd1ac193e359581b76b99
Marko Vitković <mavitkov@vsite.hr>
2020-03-18 20:15:44 +0100

    ispravak

--- TestGitRebase/Program.cs
+++ TestGitRebase/Program.cs
@@ -5,23 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TestGitRebase
-{
-	static void Main(string[] args) 
+{	class Program
 	{
-		Nastavnik();
-		Marko();
+		static void Main(string[] args)
+		{
+			Nastavnik();
+			Marko();
 
-		Console.ReadKey(false);
-	}
-	static void Nastavnik()
-	{
-		Console.WriteLine("Course title: OOM");
-	}
+			Console.ReadKey(false);
+		}
+		static void Nastavnik()
+		{
+			Console.WriteLine("Course title: OOM");
+		}
 
-	static void Marko()
-	{
-		Console.WriteLine("Course title: OOM");
-		Console.WriteLine("My Name is Marko.");
+		static void Marko()
+		{
+			Console.WriteLine("Course title: OOM");
+			Console.WriteLine("My Name is Marko.");
+		}
 	}
-
 }

e51d7b4d87ba94ee18af353ddf0b2997a2815354
Sara Ćuzele Papata <saracuzele@gmail.com>
2020-03-18 20:23:34 +0100

    Merge from remote master

--- TestGitRebase/Program.cs
+++ TestGitRebase/Program.cs
@@ -11,13 +11,36 @@ namespace TestGitRebase
         static void Main(string[] args)
         {
             Nastavnik();
+            SaraCuzelePapata();
+            AuroMazzi();
+
 
             Console.ReadKey(false);
         }
 
+        static void SaraCuzelePapata()
+        {
+            Console.WriteLine("Course title: OOM");
+            Console.WriteLine("My Name is Sara Ćuzele Papata.");
+        }
+        static void Marko()
+        {
+            Console.WriteLine("Course title: OOM");
+            Console.WriteLine("My Name is Marko.");
+        }
         static void Nastavnik()
         {
             Console.WriteLine("Course title: OOM");
         }
+
+        static void AuroMazzi()
+        {
+            Console.WriteLine("Course title: OOM");
+            Console.WriteLine("My Name is Auro Mazzi.");
+        }
+
+
     }
+
+    
 }

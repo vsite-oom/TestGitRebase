645b1f03e836e2bcc149510a6dab43495e6de6ed
esvegot <esvegot@HR-00004162>
2020-04-08 19:38:26 +0200

    Add favourite movie

--- TestGitRebase/Program.cs
+++ TestGitRebase/Program.cs
@@ -263,6 +263,7 @@ namespace TestGitRebase
             Console.WriteLine("My Name is Sven Gotal.");
             Console.WriteLine("I like soup.");
             Console.WriteLine("My favourite place is Zagreb.");
+            Console.WriteLine("My favourite movie is: Event Horizon.");
         }
     }
 }

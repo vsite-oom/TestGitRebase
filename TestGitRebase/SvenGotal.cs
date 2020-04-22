319363a0e4bd6df9ccca79e667376b9927c8b69c
esvegot <esvegot@HR-00004162>
2020-04-22 20:05:07 +0200

    Add class SvenGotal, call SvenGotal.Present() in my method.

---
+++ TestGitRebase/SvenGotal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestGitRebase
+{
+    class SvenGotal
+    {
+
+        public void Present()
+        {
+
+            Console.WriteLine("This is my class Sven.");
+
+        }
+
+
+
+    }
+}

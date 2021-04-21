4a6b128f79584a6b96729b63f1441261b854dad0
Jasmin Husić <jhsc1988@gmail.com>
2021-04-21 20:02:30 +0200

    Add message to JHusic class

--- TestGitRebase/JHusic.cs
+++ TestGitRebase/JHusic.cs
@@ -10,6 +10,7 @@ namespace TestGitRebase
         public void Message()
         {
             Console.WriteLine("I am JHusic class");
+            Console.WriteLine("This is the end of TestGitRebase");
         }
     }
 }

e2ebc9b2a3a8d1cc9a0a8906a5194665beacde29
dajergov <dadoj@DESKTOP-4PTLH1S>
2021-04-21 20:02:37 +0200

    Add message to dJergovic class

--- TestGitRebase/DJergovic.cs
+++ TestGitRebase/DJergovic.cs
@@ -10,6 +10,7 @@ namespace TestGitRebase
         public void Message()
         {
             Console.WriteLine("I am DJergovic class");
+            Console.WriteLine("This is the end of TestGitRebase");
         }
     }
 }

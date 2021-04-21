39411a4941f8d346d8c5da290367bcfc90e8f80d
Mislav <mislav.miseric@gmail.com>
2021-04-21 20:03:44 +0200

    class, student, end of TestGitRebase

---
+++ TestGitRebase/MMiseric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestGitRebase
+{
+    class MMiseric
+    {
+        public void Message()
+        {
+            Console.WriteLine("I am Mislav Mišerić.");
+            Console.WriteLine("I am a student of VSITE, fourth year.");
+            Console.WriteLine("This is the end of TestGitRebase");
+        }
+    }
+}

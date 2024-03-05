217f05d3fee96e278691dae25f4e6be7931bdc26
Julijan Šribar <julijan.sribar@gmail.com>
2024-03-05 20:21:20 +0100

    Add students

--- TestGitRebase/Program.cs
+++ TestGitRebase/Program.cs
@@ -4,6 +4,46 @@
     {
         static void Main(string[] args)
         {
+            // Kristian Bolarić
+
+            // Mislav Ćavarušić
+
+            // Benjamin Gajski
+
+            // Josip Grabušić
+
+            // Petar Huljek
+
+            // Hrvoje Janđel
+
+            // Mario Knežević
+
+            // Mihael Krpes
+
+            // Filip Leš
+
+            // Antonio Maletić
+
+            // Kristian Mesić
+
+            // Marko Pavić
+
+            // Sonja Perković
+
+            // Đuro Rukavina
+
+            // Ivan Šelendić
+
+            // Šime Šprljan
+
+            // Julijan Šribar
+
+            // Adrian Stančer
+
+            // Vlado Tonković
+
+            // nastavnik
+
         }
     }
 }

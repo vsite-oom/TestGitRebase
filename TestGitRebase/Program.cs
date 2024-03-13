67ff043b2d6fce542b35473e4fddb43ed6834c37
Julijan Sribar <julijan.sribar@gmail.com>
2024-03-13 21:07:45 +0100

    Add name

--- TestGitRebase/Program.cs
+++ TestGitRebase/Program.cs
@@ -37,7 +37,7 @@
             // Šime Šprljan
 
             // Julijan Šribar
-
+            Console.WriteLine("My name is Julijan");
             // Adrian Stančer
 
             // Vlado Tonković
